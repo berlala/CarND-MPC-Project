b297fdebd4747624721230309323a8534cf78e62
Levin Jian <jianzhirong@gmail.com>
2017-05-20 14:08:40 +0800

    update solver computation limit to 0.05

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -281,9 +281,9 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs,vector<d
 	// magnitude.
 	options += "Sparse  true        forward\n";
 	options += "Sparse  true        reverse\n";
-	// NOTE: Currently the solver has a maximum time limit of 0.5 seconds.
+	// NOTE: Currently the solver has a maximum time limit of 0.05 seconds.
 	// Change this as you see fit.
-	options += "Numeric max_cpu_time          0.5\n";
+	options += "Numeric max_cpu_time          0.05\n";
 
 	// place to return solution
 	CppAD::ipopt::solve_result<Dvector> solution;
